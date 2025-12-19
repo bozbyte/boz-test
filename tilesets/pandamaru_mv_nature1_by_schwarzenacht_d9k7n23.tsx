a1eacfdd5b0a7fae5e73305133a05e8a2e6ef0c6
bozbyte <144847001+bozbyte@users.noreply.github.com>
2025-12-19 04:59:47 +0300

    tileset location
    exported tile set to tile set folder

---
+++ tilesets/pandamaru_mv_nature1_by_schwarzenacht_d9k7n23.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pandamaru_mv_nature1_by_schwarzenacht_d9k7n23" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="../../../../Desktop/WA maps/BAWES-virtual-master/tilesets/pandamaru_mv_nature1_by_schwarzenacht_d9k7n23.png" width="768" height="768"/>
+</tileset>
