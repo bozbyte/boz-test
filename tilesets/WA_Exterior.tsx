ac9fad6dc94c44204b27739c8ef6df6c20a80c7a
bozbyte <144847001+bozbyte@users.noreply.github.com>
2025-12-22 03:03:49 +0300

    tt
    tt

--- tilesets/WA_Exterior.tsx
+++ tilesets/WA_Exterior.tsx
@@ -1,6 +1,42 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="WA_Exterior" tilewidth="32" tileheight="32" tilecount="850" columns="25">
  <image source="WA_Exterior.png" width="800" height="1088"/>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="1000"/>
+   <frame tileid="13" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="1000"/>
+   <frame tileid="9" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <animation>
+   <frame tileid="34" duration="1000"/>
+   <frame tileid="37" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <animation>
+   <frame tileid="38" duration="1000"/>
+   <frame tileid="35" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="59">
+  <animation>
+   <frame tileid="59" duration="1000"/>
+   <frame tileid="62" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="60" duration="1000"/>
+   <frame tileid="63" duration="1000"/>
+  </animation>
+ </tile>
  <tile id="533">
   <animation>
    <frame tileid="533" duration="1000"/>
